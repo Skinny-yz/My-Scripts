a741c779c98990e37c0ae46414a4011bfd122045
Skinny-yz <luizfelipe.88jka@gmail.com>
2024-08-10 02:06:59 -0300

    Update Hwids-NoKey.lua

--- TiredOfKeySystem/Hwids-NoKey.lua
+++ TiredOfKeySystem/Hwids-NoKey.lua
@@ -1,4 +1,5 @@
 return {
-"", --Skinny(Perm)
+"b4ec6d5b-117f-42be-a8e3-b64385f07021", --Skinny(Perm) [MSI APPPlayer]
+"0790cda0-5e19-49ec-aa36-291384a2b198", -- Skinny(Perm [MuMu Player]
 }
---0790cda0-5e19-49ec-aa36-291384a2b198
+
