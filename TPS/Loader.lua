2b2c6910dbc4e1fd0ffb6196e85ffea96c333c1c
Skinny-yz <luizfelipe.88jka@gmail.com>
2024-09-06 11:02:17 -0300

    Update Loader.lua

--- TPS/Loader.lua
+++ TPS/Loader.lua
@@ -1,1 +1,2 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/Skinny-yz/My-Scripts/main/TPS/Loader.lua"))()
+--3232323232
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/Skinny-yz/My-Scripts/main/TPS/Chooser.lua"))()
