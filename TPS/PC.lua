b970b5c3acc8833b1998b3de41e5adc7dd9cb1ee
Skinny-yz <luizfelipe.88jka@gmail.com>
2024-10-17 01:09:53 -0300

    Update PC.lua

--- TPS/PC.lua
+++ TPS/PC.lua
@@ -8,29 +8,48 @@
         discord.gg/gV7eJT9wp4
          [*] Version 3.7
 ]]
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-local UserInputService = game:GetService("UserInputService")
-local StarterGui = game:GetService("StarterGui")
-local function isMobile()
-    return UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled
+rconsolecreate()
+local key = loadstring(game:HttpGet('https://pastebin.com/raw/npLfzrFH'))()
+function begin()
+  rconsoleprint('@@GREEN@@') 
+  rconsoleprint([[
+ __      __       _  __ _           _   _                _____           _                 
+ \ \    / /      (_)/ _(_)         | | (_)              / ____|         | |                
+  \ \  / /__ _ __ _| |_ _  ___ __ _| |_ _  ___  _ __   | (___  _   _ ___| |_ ___ _ __ ___  
+   \ \/ / _ \ '__| |  _| |/ __/ _` | __| |/ _ \| '_ \   \___ \| | | / __| __/ _ \ '_ ` _ \ 
+    \  /  __/ |  | | | | | (_| (_| | |_| | (_) | | | |  ____) | |_| \__ \ ||  __/ | | | | |
+     \/ \___|_|  |_|_| |_|\___\__,_|\__|_|\___/|_| |_| |_____/ \__, |___/\__\___|_| |_| |_|
+                                                                __/ |                      
+                                                               |___/                                                                                              
+    ]])
+    rconsoleprint('@@WHITE@@')
+    rconsolename("Verification System - TPS Script")
+    rconsoleprint("Get key in: discord.gg/gV7eJT9wp4\n")
+    rconsoleprint('@@WHITE@@') 
+    rconsoleprint("    Enter Key:\n")
+    local input = rconsoleinput()
+    if input == key then
+        rconsoleclear()
+        rconsoleprint('@@LIGHT_GREEN@@')
+        rconsolename("Main Script")
+rconsoleprint([[
+   _____           _       _     ______                     _   _                         
+  / ____|         (_)     | |   |  ____|                   | | (_)                        
+ | (___   ___ _ __ _ _ __ | |_  | |__  __  _____  ___ _   _| |_ _ _ __   __ _             
+  \___ \ / __| '__| | '_ \| __| |  __| \ \/ / _ \/ __| | | | __| | '_ \ / _` |            
+  ____) | (__| |  | | |_) | |_  | |____ >  <  __/ (__| |_| | |_| | | | | (_| |  _   _   _ 
+ |_____/ \___|_|  |_| .__/ \__| |______/_/\_\___|\___|\__,_|\__|_|_| |_|\__, | (_) (_) (_)
+                    | |                                                  __/ |            
+                    |_|                                                 |___/                                 
+]])
+rconsoleprint("Welcome! Enjoy\n")
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/Skinny-yz/My-Scripts/768c886882227e61bdb651d85e981ec1024e0e94/TPS/PC.lua"))()
+    else
+        rconsoleprint('@@RED@@')
+        rconsoleprint("Incorrect key!\n")
+        wait(2)
+        rconsoleclear()
+        begin()
+    end
 end
-if not isMobile() then
-    player:Kick("Stop trying to bypass the key system.")
-end
-local function WSkinny(text, duration)
-    StarterGui:SetCore("SendNotification", {
-        Title = "About Premium",
-        Text = text,
-        Duration = duration,
-        Icon = "rbxassetid://78710902912785"
-    })
-end
-wait(0.1)
-WSkinny("The premium script has been officially released for a 3-day trial!", 15)
-wait(0.1)
-WSkinny("Join our discord to buy, it only costs 20 Robux", 15)
-wait(0.1)
-WSkinny("It has +15 reactions and many more functions than the free script and is better than all existing scripts.", 15)
-setclipboard("discord.gg/gV7eJT9wp4")
- loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIlIIlIIIl42344LLLIlIlIl321dOV2llIl/8798787KSFSDFSD/refs/heads/main/Mobile.lua"))()
+begin()
